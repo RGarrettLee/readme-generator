cd2cb695f6038ef854f2d96778852445e6f51ab3
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-06-08 22:17:43 -0400

    created writeToFile function

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}.md`, data, (err) => {
+        err ? console.log(err) : console.log('README Generated');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
