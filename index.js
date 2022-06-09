25c37b74c083f163d8aef07d143d6c000b3bf9fa
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-06-08 22:22:43 -0400

    created initial questions for user

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const fs = require('fs');
 const markdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the name of your project?', 
+'Give a description of your project', 
+'Installation info', // start of table of contents stuff
+'Usage info', // each section must actually link to their respective parts
+'Contribution guidelines', // later make each part of this optional with a checkbox
+'Test instructions', // end of table of contents items 
+'Choose a license for your project', // list & get license badge
+'Enter your github username', // put in questions section and link users github
+'Enter your email address', // put in questions section
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -15,7 +24,13 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
+    inquirer
+        .prompt([
 
+        ])
+        .then(response => {
+
+        })
 }
 
 // Function call to initialize app
